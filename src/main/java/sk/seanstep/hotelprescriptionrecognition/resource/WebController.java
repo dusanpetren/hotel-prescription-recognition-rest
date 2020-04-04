40b0fa569cde196ccce4a8f5e6a4d44ea3485fc8
dusannnn <dusanpetren@outlook.com>
2020-04-04 20:20:23 +0200

    Update names

--- src/main/java/sk/seanstep/hotelprescriptionrecognition/resource/WebController.java
+++ src/main/java/sk/seanstep/hotelprescriptionrecognition/resource/WebController.java
@@ -1,7 +1,9 @@
 package sk.seanstep.hotelprescriptionrecognition.resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,5 +19,11 @@ public class WebController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@GetMapping("/code/{code}")
+	public String codes(@PathVariable String code, Model model) {
+		model.addAttribute("generatedCode", code);
+		return "prescription-list";
 	}
 }
