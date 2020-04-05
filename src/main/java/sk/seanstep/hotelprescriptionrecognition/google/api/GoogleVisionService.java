53608ef658a70e18cab4c89be9909efa83d27cbf
dusannnn <dusanpetren@outlook.com>
2020-04-05 11:02:16 +0200

    Added 404 error page html

--- src/main/java/sk/seanstep/hotelprescriptionrecognition/google/api/GoogleVisionService.java
+++ src/main/java/sk/seanstep/hotelprescriptionrecognition/google/api/GoogleVisionService.java
@@ -1,6 +1,7 @@
 package sk.seanstep.hotelprescriptionrecognition.google.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,12 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class GoogleVisionService {
+
+	@Value("google.api.url")
+	private String googleApiUrl;
+
+	@Value("google.api.key")
+	private String googleApiKey;
 
 	@Autowired
 	private RestTemplate restTemplate;
