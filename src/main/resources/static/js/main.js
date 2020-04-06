f06a5595d4eb1e03cb9a59d6578318d43dc7946c
dusannnn <dusanpetren@outlook.com>
2020-04-06 23:24:45 +0200

    Bug fix

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,9 +4,6 @@ const HTTP_STATUS_OK = "OK";
 var stompClient = null;
 var latestGeneratedCode = null;
 
-var websocketDestination = $('#generatedCode2').text();
-
-
 function initializeAndSubscripeWebSocketOnMobile() {
     console.log("connect with stomp: " + (stompClient != null));
     if (stompClient === null) {
@@ -53,12 +50,13 @@ function disconnect() {
 
 function joinWebSocketSession() {
     console.log("joinWebSocketSession");
+    var websocketDestination = $('#generatedCode2').text();
     stompClient.send("/api/join" + websocketDestination, {}, JSON.stringify({'imageBase64': name}));
 }
 
 function sendName() {
     var name = $("#name").val()
-
+    var websocketDestination = $('#generatedCode2').text();
     stompClient.send("/api/add/" + websocketDestination, {}, JSON.stringify({'imageBase64': name}));
 }
 
