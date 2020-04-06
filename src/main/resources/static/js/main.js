aa97b2863256fb907227bbf86f84a8db0bf696a2
Kubo <sabokubo@gmail.com>
2020-04-06 19:24:30 +0200

    fucked up stringify, added new way of connecting

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -49,15 +49,14 @@ function sendName() {
     stompClient.send("/api/add/", $("#name").val());
 }
 
-$( document ).ready(function() {
-
+$(document).ready(function () {
     $.ajax({
         url: '/code/generate',
         type: 'GET',
         success: function (response) {
             latestGeneratedCode = response;
             console.log("generated Id: " + latestGeneratedCode);
-            $('#qrcode').qrcode("https://presreco-rest.herokuapp.com/web/code/"+ latestGeneratedCode);
+            $('#qrcode').qrcode("https://presreco-rest.herokuapp.com/web/code/`" + latestGeneratedCode);
             var canvas = $('#qrcode canvas');
             console.log(canvas);
         },
@@ -73,40 +72,19 @@ function generateCode() {
         type: 'GET',
         success: function (response) {
             latestGeneratedCode = response;
-            $( "canvas").get( 0 ).remove();
-            $('#qrcode').qrcode("https://presreco-rest.herokuapp.com/web/code/"+ latestGeneratedCode);
+            $("canvas").get(0).remove();
+            $('#qrcode').qrcode("https://presreco-rest.herokuapp.com/web/code/" + latestGeneratedCode);
             console.log("generated Id: " + latestGeneratedCode);
             return response;
         },
-        error : function(request, textStatus, errorThrown) {
+        error: function (request, textStatus, errorThrown) {
             alert(errorThrown);
         }
     });
 }
 
-function generateBase64fromImage() {
-    function toDataURL(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                callback(reader.result);
-            }
-            reader.readAsDataURL(xhr.response);
-        };
-        xhr.open('GET', url);
-        xhr.responseType = 'blob';
-        xhr.send();
-    }
-
-    toDataURL('../img/hotelHandWriting', function(dataUrl) {
-        console.log('RESULT:', dataUrl)
-    })
-}
-
 function redirectToGeneratedId() {
-    console.log("ggoing to space: " + latestGeneratedCode);
-    window.location.href = "/web/code/" + latestGeneratedCode;
+    window.location.href = latestGeneratedCode;
 }
 
 function showGreeting(message) {
