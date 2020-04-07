7f1b374f9811cf52f9c7f7d400d9ca1567a5e7a1
dusannnn <dusanpetren@outlook.com>
2020-04-07 12:45:17 +0200

    Add toastr

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -72,6 +72,24 @@ function sendImageBase64ToServer(base64) {
         data: JSON.stringify({'imageBase64': base64}),
         contentType: 'application/json',
         success: function (response) {
+            toastr.options = {
+                "closeButton": false,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": false,
+                "positionClass": "toast-top-full-width",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            };
+            toastr.success('Poslano!');
             resolvedMessageFromGoogle = response;
             sendToWebSocket(resolvedMessageFromGoogle);
         },
@@ -98,6 +116,24 @@ function startIndex() {
 };
 
 function redirectToGeneratedId() {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+    toastr.success('Připojeno!');
     window.location.href = "/web/code/" + latestGeneratedCode;
 }
 
